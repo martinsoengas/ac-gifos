7898d7f8269da48091b3029529f37399ceb3dda7
Martin Soengas <martinsoengas@Martins-Macbook-Pro.local>
2020-09-17 15:50:03 -0300

    view more added to search results

--- js/trending.js
+++ js/trending.js
@@ -1,6 +1,6 @@
 import { config } from "./config.js";
 
-let OFFSET_COUNTER = 0;
+let OFFSET_COUNTER = 12;
 /* -----------------------------  MAIN SEARCH ----------------------------- */
 const inputMain = document.getElementById("search-main");
 const containerSearch = document.getElementById("container-search");
@@ -8,23 +8,38 @@ const p = document.createElement("p");
 const suggestionsList = document.getElementById("suggestions-list");
 const iconOnSearch = document.getElementById("icon-onsearch");
 const searchResults = document.getElementById('search-results')
+const searchTitle = document.getElementById('search-title')
+const viewMore = document.getElementById('view-more-container')
 
 async function searchExecute(searchTerm) {
   if (searchTerm !== "") {
-    let urlSearch = `${config.baseUrl}/gifs/search?api_key=${config.API_KEY}&q=${searchTerm}&limit=12`;
+    let urlSearch = `${config.baseUrl}/gifs/search?api_key=${config.API_KEY}&q=${searchTerm}&limit=${OFFSET_COUNTER}`;
     const response = await fetch(urlSearch);
     const search = await response.json();
 
-    searchResults.innerHTML = "";
+    if (search !== "") {
+      OFFSET_COUNTER = OFFSET_COUNTER + 12;
 
-    search.data.forEach(e => {
-      let gifImgs = e.images.original.url;
-      console.log(gifImgs);
-      const gifImgContainer = document.createElement('img')
-      gifImgContainer.classList.add('img-gifs')
-      gifImgContainer.setAttribute('src', gifImgs)
-      searchResults.appendChild(gifImgContainer)
-    })
+      searchTitle.innerHTML = "";
+      searchResults.innerHTML = "";
+      iconOnSearch.classList.remove("icon-onsearch-visible");
+
+      const pSearchTitle = document.createElement('p')
+      pSearchTitle.textContent = capitalizeFirstLetter(searchTerm)
+      pSearchTitle.classList.add('p-search-title')
+      searchTitle.appendChild(pSearchTitle);
+
+      viewMore.classList.remove('display-none')
+
+      search.data.forEach(e => {
+        let gifImgs = e.images.original.url;
+        const gifImgContainer = document.createElement('img')
+        gifImgContainer.classList.add('img-gifs')
+        gifImgContainer.setAttribute('loading', 'lazy')
+        gifImgContainer.setAttribute('src', gifImgs)
+        searchResults.appendChild(gifImgContainer)
+      });
+    }
   }
 }
 
@@ -41,7 +56,6 @@ async function searchSuggestions(searchTerm) {
     suggestionsList.appendChild(hrLine);
     iconOnSearch.classList.add("icon-onsearch-visible");
 
-
     for (let i = 0; i <= 3; i++) {
       const divIcon = document.createElement("div");
       divIcon.classList.add("div-icon");
@@ -57,15 +71,21 @@ async function searchSuggestions(searchTerm) {
       suggestionsList.appendChild(divIcon);
 
       liSuggestions.textContent = capitalizeFirstLetter(search.data[i].name);
-
-
     }
+
   } else {
     suggestionsList.innerHTML = "";
     searchResults.innerHTML = "";
     iconOnSearch.classList.remove("icon-onsearch-visible");
+    searchTitle.innerHTML = "";
+    viewMore.classList.add('display-none')
+    OFFSET_COUNTER = 12;
   }
 }
+
+viewMore.addEventListener('click', () => {
+  searchExecute(inputMain.value);
+});
 
 let ul = document.getElementById("suggestions-list");
 
@@ -91,17 +111,17 @@ inputMain.addEventListener("keyup", () => {
   }, 500);
 });
 
-/* inputMain.addEventListener("focusout", (event) => {
+inputMain.addEventListener("focusout", (event) => {
   setTimeout(() => {
-    
-
+    suggestionsList.innerHTML = "";
   }, 300)
-}); */
+});
 
-
+function toggleDisplay(e) {
+  e.classList.toggle('display-none')
+}
 
 // TRENDING TERMS
-
 const trendingFiveContainer = document.getElementById("trending-five");
 const pFive = document.createElement("p");
 pFive.classList.add("trending-words");
