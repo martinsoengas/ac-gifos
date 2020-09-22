8cd18a89e604a24eb51ab018bddf401dfe919309
Martin Soengas <martinsoengas@Martins-Macbook-Pro.local>
2020-09-22 00:39:17 -0300

    rafactoring

--- js/script.js
+++ js/script.js
@@ -6,35 +6,24 @@ let isDarkMode = false;
 // function sticky navbar
 const navbar = document.getElementById("navbar");
 let sticky = navbar.offsetTop;
-const createGif = document.getElementById("create-gif-border");
-const createEmpty = document.getElementById("create-empty");
 const searchNav = document.getElementById("search-nav");
 const viewMore = document.getElementById('view-more');
 
 window.addEventListener("scroll", () => {
   if (window.pageYOffset >= sticky) {
     if (isDarkMode) {
-      // createEmpty.classList.add("visibility-hidden");
-      // createGif.classList.add("visibility-hidden");
       navbar.classList.add("sticky-dark");
       navbar.classList.remove("sticky");
-      // createEmpty.classList.add('container-empty-desktop')
       searchNav.classList.remove("display-none");
     } else {
-      // createEmpty.classList.add("visibility-hidden");
-      // createGif.classList.add("visibility-hidden");
       navbar.classList.add("sticky");
       navbar.classList.remove("sticky-dark");
-      // createEmpty.classList.add('container-empty-desktop')
       searchNav.classList.remove("display-none");
     }
   } else {
-    // createEmpty.classList.remove("visibility-hidden");
-    // createGif.classList.remove("visibility-hidden");
     navbar.classList.remove("sticky-dark");
     navbar.classList.remove("sticky");
     searchNav.classList.add("display-none");
-    // createEmpty.classList.remove('container-empty-desktop')
   }
 });
 
@@ -67,25 +56,12 @@ function darkTheme() {
     .getElementById("container-trending")
     .classList.toggle("dark-container-trending");
   document.getElementById("nav-ul").classList.toggle("dark-nav-ul");
-  // document
-  //   .getElementsByTagName("footer")[0]
-  //   .classList.toggle("dark-border-bottom");
-
-  // let navLI = document.getElementsByClassName("nav-item");
-  // for (let i = 0; i < navLI.length; i++) {
-  //   navLI[i].classList.toggle("dark-text-important");
-  // }
 
   if (darkThemeButton.textContent === "Modo Nocturno") {
     darkThemeButton.textContent = "Modo Diurno";
   } else {
     darkThemeButton.textContent = "Modo Nocturno";
   }
-
-  // let allP = document.getElementsByTagName("p");
-  // for (let i = 0; i < allP.length; i++) {
-  //   allP[i].classList.toggle("dark-text");
-  // }
 
   let iconSearch = document.querySelectorAll(".icon-search");
   iconSearch.forEach((element) => {
@@ -120,33 +96,4 @@ function darkTheme() {
   } else if ("img/button-close-dark.svg") {
     burger.setAttribute("src", "img/button-close.svg");
   }
-
-  // let createGif = document.getElementById("create-svg-fill");
-  // let createGifAtt = createGif.getAttribute("fill");
-
-  // if (createGifAtt === "#572EE5") {
-  //   createGif.setAttribute("fill", "#FFFFFF");
-  // } else {
-  //   createGif.setAttribute("fill", "#572EE5");
-  // }
-
-  // const viewMoreAttribute = viewMore.getAttribute('src');
-
-  // console.log(viewMoreAttribute)
-
-  // if (viewMoreAttribute === "img/CTA-ver-mas.svg") {
-  //   viewMore.setAttribute('src', 'img/CTA-ver+modo-noc.svg')
-  // } else if (viewMoreAttribute === "img/CTA-ver+modo-noc.svg") {
-  //   viewMore.setAttribute('src', 'img/CTA-ver-mas.svg')
-  // }
-
-  // document
-  //   .getElementById("create-gif-border")
-  //   .classList.toggle("dark-create-gif-border");
 }
-// dark theme end
-// if (navbar.classList.contains("sticky")) {
-//   createGif.classList.add("visibility-hidden");
-// } else {
-//   createGif.classList.remove("visibility-hidden");
-// }
